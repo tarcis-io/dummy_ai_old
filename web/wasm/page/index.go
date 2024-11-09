9ef1bcca9e03d0888d66f256e4146c6ec196246b
Tarcisio Oliveira <t@rcis.io>
2024-11-09 13:07:34 -0300

    update

--- web/wasm/page/index.go
+++ web/wasm/page/index.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"syscall/js"
+)
+
 func main() {
+
+	h2 := js.Global().Get("document").Call("createElement", "h2")
+	h2.Set("innerHTML", "Hello World, DummyAI!")
+
+	body := js.Global().Get("document").Get("body")
+	body.Call("appendChild", h2)
 }

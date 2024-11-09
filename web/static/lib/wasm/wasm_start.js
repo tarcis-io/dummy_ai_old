fe0f93473cab9ba0a81821d93127b9e01281170b
Tarcisio Oliveira <t@rcis.io>
2024-11-09 00:06:59 -0300

    update

---
+++ web/static/lib/wasm/wasm_start.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const wasmStart = function(wasmFile) {
+
+	const go = new Go();
+	WebAssembly.instantiateStreaming(fetch(wasmFile), go.importObject).then(webAssemblyInstantiatedSource => go.run(webAssemblyInstantiatedSource).instance);
+};

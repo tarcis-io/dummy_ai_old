0a6830f08da59cd0bea440816d916ba1c0af458d
Tarcisio Oliveira <t@rcis.io>
2024-11-24 00:12:58 -0300

    update

--- web/wasm/component/camera.go
+++ web/wasm/component/camera.go
@@ -6,8 +6,21 @@ import (
 
 func NewCamera() js.Value {
 
-	camera := js.Global().Get("document").Call("createElement", "div")
-	camera.Set("innerHTML", "Camera")
+	return NewCameraLoading()
+}
 
-	return camera
+func NewCameraLoading() js.Value {
+
+	cameraLoading := js.Global().Get("document").Call("createElement", "div")
+	cameraLoading.Set("innerHTML", "CameraLoading")
+
+	return cameraLoading
+}
+
+func NewCameraError() js.Value {
+
+	cameraError := js.Global().Get("document").Call("createElement", "div")
+	cameraError.Set("innerHTML", "CameraError")
+
+	return cameraError
 }

4530b197f6cfa46f244d3004a815d0c9b0e9e3f5
Tarcisio Oliveira <t@rcis.io>
2024-11-27 23:26:08 -0300

    update

--- web/wasm/util/message.go
+++ web/wasm/util/message.go
@@ -9,6 +9,27 @@ var (
 		English: {
 			App: "DummyAI",
 		},
+		Spanish: {
+			App: "DummyAI",
+		},
+		Portuguese: {
+			App: "DummyAI",
+		},
 	}
 	messages = allMessages[language]
 )
+
+func AllMessages() map[string]map[int]string {
+
+	return allMessages
+}
+
+func Messages() map[int]string {
+
+	return messages
+}
+
+func Message(key int) string {
+
+	return messages[key]
+}

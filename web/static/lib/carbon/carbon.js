9b5873fef3ff346314dde79fdd8bf9839bbf323b
Tarcisio Oliveira <t@rcis.io>
2024-12-07 01:04:58 -0300

    update

--- web/static/lib/carbon/carbon.js
+++ web/static/lib/carbon/carbon.js
@@ -1,2 +1,2 @@
-import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/inline-loading.min.js';
+import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/loading.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/tile.min.js';

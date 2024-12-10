2d33f438c6f0a4ed7ce7baa09916382874713eb9
Tarcisio Oliveira <t@rcis.io>
2024-12-10 00:36:56 -0300

    update

--- web/static/lib/carbon/carbon.js
+++ web/static/lib/carbon/carbon.js
@@ -2,3 +2,4 @@
 
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/loading.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/tile.min.js';
+import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/ui-shell.min.js';

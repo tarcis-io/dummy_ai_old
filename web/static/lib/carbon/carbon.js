90e70ad955c6573aefe3f5db64d57cb5c5e583db
Tarcisio Oliveira <t@rcis.io>
2024-12-09 23:03:48 -0300

    update

--- web/static/lib/carbon/carbon.js
+++ web/static/lib/carbon/carbon.js
@@ -1,2 +1,4 @@
+'use strict';
+
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/loading.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/tile.min.js';

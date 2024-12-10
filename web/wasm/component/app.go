d6ea8d3adc45cd10f92d0a3762c82b9fb06f2b7b
Tarcisio Oliveira <t@rcis.io>
2024-12-10 01:14:29 -0300

    update

--- web/wasm/component/app.go
+++ web/wasm/component/app.go
@@ -11,15 +11,9 @@ import (
 func CreateApp(page js.Value) js.Value {
 
 	div := js.Global().Get("document").Call("createElement", "div")
-	div.Get("classList").Call("add", "cds-theme-zone-g100")
-	div.Get("style").Set("font-family", "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif")
-	div.Get("style").Set("position", "fixed")
-	div.Get("style").Set("top", "0")
-	div.Get("style").Set("right", "0")
-	div.Get("style").Set("bottom", "0")
-	div.Get("style").Set("left", "0")
-	div.Get("style").Set("background-color", "var(--cds-background)")
-	div.Get("style").Set("color", "var(--cds-text-primary)")
+	div.Set("id", "app")
+	div.Set("className", "cds-theme-zone-g100")
+	div.Call("appendChild", createAppHeader())
 	div.Call("appendChild", page)
 
 	return div

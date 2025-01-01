8ffcc8b65866fafab8e4b2b7bfd66823157c90c2
Tarcisio Oliveira <t@rcis.io>
2025-01-01 17:14:11 -0300

    update

--- web/static/lib/carbon/carbon.js
+++ web/static/lib/carbon/carbon.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/button.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/loading.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/notification.min.js';
 import 'https://1.www.s81c.com/common/carbon/web-components/tag/v2/latest/stack.min.js';

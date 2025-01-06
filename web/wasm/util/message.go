8d830e85db51d45b95591b819dddf38e45012afa
Tarcisio Oliveira <t@rcis.io>
2025-01-06 15:18:47 -0300

    update

--- web/wasm/util/message.go
+++ web/wasm/util/message.go
@@ -2,6 +2,10 @@ package util
 
 const (
 	app = iota
+	cameraErrorNotAllowedText
+	cameraErrorNotAllowedTitle
+	cameraErrorNotFoundText
+	cameraErrorNotFoundTitle
 	cameraErrorNotSupportedText
 	cameraErrorNotSupportedTitle
 	cameraLoading
@@ -12,6 +16,10 @@ var (
 	allMessages = map[string]map[int]string{
 		English: {
 			app:                          "DummyAI",
+			cameraErrorNotAllowedText:    "",
+			cameraErrorNotAllowedTitle:   "Camera not allowed",
+			cameraErrorNotFoundText:      "",
+			cameraErrorNotFoundTitle:     "Camera not found",
 			cameraErrorNotSupportedText:  "Please check your device settings",
 			cameraErrorNotSupportedTitle: "Camera not supported",
 			cameraLoading:                "Loading...",
@@ -19,6 +27,10 @@ var (
 		},
 		Spanish: {
 			app:                          "DummyAI",
+			cameraErrorNotAllowedText:    "",
+			cameraErrorNotAllowedTitle:   "",
+			cameraErrorNotFoundText:      "",
+			cameraErrorNotFoundTitle:     "",
 			cameraErrorNotSupportedText:  "Verifique la configuración de su dispositivo",
 			cameraErrorNotSupportedTitle: "Cámara no soportada",
 			cameraLoading:                "Cargando...",
@@ -26,6 +38,10 @@ var (
 		},
 		Portuguese: {
 			app:                          "DummyAI",
+			cameraErrorNotAllowedText:    "",
+			cameraErrorNotAllowedTitle:   "",
+			cameraErrorNotFoundText:      "",
+			cameraErrorNotFoundTitle:     "",
 			cameraErrorNotSupportedText:  "Verifique as configurações do seu dispositivo",
 			cameraErrorNotSupportedTitle: "Câmera não suportada",
 			cameraLoading:                "Carregando...",
@@ -38,6 +54,26 @@ var (
 func App() string {
 
 	return messages[app]
+}
+
+func CameraErrorNotAllowedText() string {
+
+	return messages[cameraErrorNotAllowedText]
+}
+
+func CameraErrorNotAllowedTitle() string {
+
+	return messages[cameraErrorNotAllowedTitle]
+}
+
+func CameraErrorNotFoundText() string {
+
+	return messages[cameraErrorNotFoundText]
+}
+
+func CameraErrorNotFoundTitle() string {
+
+	return messages[cameraErrorNotFoundTitle]
 }
 
 func CameraErrorNotSupportedText() string {

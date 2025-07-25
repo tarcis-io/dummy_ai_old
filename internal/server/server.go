acf46a3e66181951cf232d7a7905bbeca33e3e74
Tarcisio Oliveira <t@rcis.io>
2025-07-25 04:23:27 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,34 +1,9 @@
 package server
 
 import (
-	"log"
-	"net/http"
 	"text/template"
-
-	"dummy_ai/internal/env"
 )
 
-type (
-	Server struct {
-		router       *http.ServeMux
-		httpTemplate *template.Template
-	}
+var (
+	httpTemplate = template.Must(template.ParseFiles("template.html"))
 )
-
-func (s *Server) RegisterFileServer(urlPath, dir string) {
-	s.router.Handle(urlPath, http.StripPrefix(urlPath, http.FileServer(http.Dir(dir))))
-}
-
-func (s *Server) ListenAndServe() {
-	err := http.ListenAndServe(env.ServerAddress(), s.router)
-	if err != nil {
-		log.Fatalf("FATAL: Failed to start server: %v", err)
-	}
-}
-
-func New() *Server {
-	return &Server{
-		router:       http.NewServeMux(),
-		httpTemplate: template.Must(template.ParseFiles("template.html")),
-	}
-}

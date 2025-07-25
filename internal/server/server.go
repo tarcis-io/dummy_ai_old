4df96208799683a0d88a60d8a85ccae19579ed58
Tarcisio Oliveira <t@rcis.io>
2025-07-25 04:26:29 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,20 @@
 package server
 
 import (
+	"log"
+	"net/http"
 	"text/template"
+
+	"dummy_ai/internal/env"
 )
 
 var (
 	httpTemplate = template.Must(template.ParseFiles("template.html"))
 )
+
+func Run() {
+	err := http.ListenAndServe(env.ServerAddress(), nil)
+	if err != nil {
+		log.Fatalf("FATAL: Failed to start server: %v", err)
+	}
+}

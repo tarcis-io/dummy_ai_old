351d33e0bef9213cdf0045106c05082d0e7e7ba7
Tarcisio Oliveira <t@rcis.io>
2025-07-25 02:33:10 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,8 +6,16 @@ import (
 )
 
 type (
-	server struct {
+	Server struct {
 		router       *http.ServeMux
 		httpTemplate *template.Template
 	}
 )
+
+func New() *Server {
+	router := http.NewServeMux()
+	return &Server{
+		router:       router,
+		httpTemplate: template.Must(template.ParseFiles("template.html")),
+	}
+}

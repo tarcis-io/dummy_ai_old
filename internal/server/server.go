249ba571e8d6501d9b94a059fdb77ddd4a0affba
Tarcisio Oliveira <t@rcis.io>
2025-07-26 04:39:07 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,6 +16,8 @@ type (
 )
 
 var (
+	staticFileServer = http.FileServer(http.Dir("./static"))
+
 	//go:embed template.html
 	httpTemplateFS embed.FS
 	httpTemplate   = template.Must(template.ParseFS(httpTemplateFS, "template.html"))
@@ -23,18 +25,23 @@ var (
 
 func Run() {
 	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderPage(w, &page{
-			wasmPath: "/wasm/home.wasm",
-		})
-	})
-	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("/", requestHandler)
 	serverAddress := env.ServerAddress()
 	log.Printf("INFO: Server starting on %s", serverAddress)
 	err := http.ListenAndServe(serverAddress, router)
 	if err != nil {
 		log.Fatalf("FATAL: Failed to start server: %v", err)
 	}
+}
+
+func requestHandler(w http.ResponseWriter, r *http.Request) {
+	requestPath := r.URL.Path
+	if requestPath == "/" {
+		renderPage(w, &page{
+			wasmPath: "/wasm/home.wasm",
+		})
+	}
+	staticFileServer.ServeHTTP(w, r)
 }
 
 func renderPage(w http.ResponseWriter, p *page) {

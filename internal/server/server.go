8176939879a27dd493e9d5b8917a18ea18b180c1
Tarcisio Oliveira <t@rcis.io>
2025-07-26 03:00:49 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,18 @@
 package server
 
+import (
+	"log"
+	"net/http"
+
+	"dummy_ai/internal/env"
+)
+
 func Run() {
+	router := http.NewServeMux()
+	serverAddress := env.ServerAddress()
+	log.Printf("INFO: Server starting on %s", serverAddress)
+	err := http.ListenAndServe(serverAddress, router)
+	if err != nil {
+		log.Fatalf("FATAL: Failed to start server: %v", err)
+	}
 }

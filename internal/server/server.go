c7524184744da8200cc06a9ab5d62e672f63d21d
Tarcisio Oliveira <t@rcis.io>
2025-07-26 02:14:33 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bytes"
 	"embed"
 	"log"
 	"net/http"
@@ -28,4 +29,21 @@ func Run() {
 	if err != nil {
 		log.Fatalf("FATAL: Failed to start server: %v", err)
 	}
+}
+
+func pageHandler(responseWriter http.ResponseWriter, wasmPath string, statusCode int) {
+	var buffer bytes.Buffer
+	err := httpTemplate.Execute(&buffer, wasmPath)
+	if err != nil {
+		log.Printf("ERROR: Failed to execute template %s: %v", wasmPath, err)
+		if wasmPath == wasmPathError500 {
+			http.Error(responseWriter, "An unexpected internal error occurred.", http.StatusInternalServerError)
+			return
+		}
+		pageHandler(responseWriter, wasmPathError500, http.StatusInternalServerError)
+		return
+	}
+	responseWriter.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	responseWriter.WriteHeader(statusCode)
+	buffer.WriteTo(responseWriter)
 }

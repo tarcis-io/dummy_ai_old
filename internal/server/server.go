fab603367e9b903b11bbe8a5f36ccd1ffb887abb
Tarcisio Oliveira <t@rcis.io>
2025-07-26 15:27:59 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,15 @@
 package server
 
+import (
+	"embed"
+	"text/template"
+)
+
+var (
+	//go:embed template.html
+	htmlTemplateFS embed.FS
+	htmlTemplate   = template.Must(template.ParseFS(htmlTemplateFS, "template.html"))
+)
+
 func Run() {
 }

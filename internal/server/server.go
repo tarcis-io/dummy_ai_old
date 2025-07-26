6a6c681880092911e4f13e0e65f4d8b7785a70ba
Tarcisio Oliveira <t@rcis.io>
2025-07-26 03:50:35 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,12 @@ import (
 	"dummy_ai/internal/env"
 )
 
+type (
+	page struct {
+		wasmPath string
+	}
+)
+
 var (
 	//go:embed template.html
 	httpTemplateFS embed.FS
@@ -18,10 +24,22 @@ var (
 func Run() {
 	router := http.NewServeMux()
 	router.Handle("/", http.FileServer(http.Dir("./static")))
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderPage(w, &page{
+			wasmPath: "/wasm/home.wasm",
+		})
+	})
 	serverAddress := env.ServerAddress()
 	log.Printf("INFO: Server starting on %s", serverAddress)
 	err := http.ListenAndServe(serverAddress, router)
 	if err != nil {
 		log.Fatalf("FATAL: Failed to start server: %v", err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, p *page) {
+	err := httpTemplate.Execute(w, p)
+	if err != nil {
+		http.Error(w, "500 internal server error", http.StatusInternalServerError)
 	}
 }

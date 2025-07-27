86a27344159fc421c96d68dba83c18655eb7535f
Tarcisio Oliveira <t@rcis.io>
2025-07-27 05:14:31 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,4 +1,24 @@
 package server
 
+type (
+	pageData struct {
+		title    string
+		wasmPath string
+	}
+)
+
+var (
+	pageRoutes = map[string]*pageData{
+		"/": {
+			title:    "DummyAI",
+			wasmPath: "/wasm/home.wasm",
+		},
+		"/about": {
+			title:    "DummyAI",
+			wasmPath: "/wasm/about.wasm",
+		},
+	}
+)
+
 func Run() {
 }

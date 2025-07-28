0c29a2d43ecd3e1561abe1866a21840033e2b143
Tarcisio Oliveira <t@rcis.io>
2025-07-28 18:47:12 +0000

    update

--- internal/wasm/dom/local_storage.go
+++ internal/wasm/dom/local_storage.go
@@ -6,6 +6,18 @@ type (
 	}
 )
 
+func (l *LocalStorage) SetItem(key, value string) {
+	l.Call("setItem", key, value)
+}
+
+func (l *LocalStorage) GetItem(key string) (string, bool) {
+	v := l.Call("getItem", key)
+	if v.Truthy() {
+		return v.String(), true
+	}
+	return "", false
+}
+
 func GetLocalStorage() *LocalStorage {
 	return GetWindow().LocalStorage()
 }

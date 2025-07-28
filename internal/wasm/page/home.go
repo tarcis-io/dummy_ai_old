a14d8701b115f64ad2c7f180a342103686ada6d9
Tarcisio Oliveira <t@rcis.io>
2025-07-28 17:05:27 +0000

    update

---
+++ internal/wasm/page/home.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"syscall/js"
+)
+
+func main() {
+	h2 := js.Global().Get("document").Call("createElement", "h2")
+	h2.Set("innerText", "/home.wasm")
+
+	body := js.Global().Get("document").Get("body")
+	body.Call("appendChild", h2)
+
+	select {}
+}

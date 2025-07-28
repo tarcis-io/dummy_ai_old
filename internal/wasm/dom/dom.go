ee15af78c204de64b5ab0a6920887f36f46e1bff
Tarcisio Oliveira <t@rcis.io>
2025-07-28 17:21:35 +0000

    update

--- internal/wasm/dom/dom.go
+++ internal/wasm/dom/dom.go
@@ -10,6 +10,26 @@ type (
 	}
 )
 
+func (d *DOM) Bool() bool {
+	return d.jsObject.Bool()
+}
+
+func (d *DOM) String() string {
+	return d.jsObject.String()
+}
+
+func (d *DOM) Int() int {
+	return d.jsObject.Int()
+}
+
+func (d *DOM) Float() float64 {
+	return d.jsObject.Float()
+}
+
+func (d *DOM) Truthy() bool {
+	return d.jsObject.Truthy()
+}
+
 func GetGlobal() DOM {
 	return DOM{
 		jsObject: js.Global(),

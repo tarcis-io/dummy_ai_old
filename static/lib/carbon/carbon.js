3cf13e10cca400ba40542fd448b86d31571665ec
Tarcisio Oliveira <t@rcis.io>
2025-07-28 23:44:46 +0000

    update

--- static/lib/carbon/carbon.js
+++ static/lib/carbon/carbon.js
@@ -1,1 +1,4 @@
 "use strict";
+
+import "https://1.www.s81c.com/common/carbon/web-components/tag/latest/button.min.js";
+import "https://1.www.s81c.com/common/carbon/web-components/tag/latest/ui-shell.min.js";

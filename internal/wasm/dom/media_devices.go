9d2fbdf5ef35800f055ffcfc9ea6b0ac2a2aed75
Tarcisio Oliveira <t@rcis.io>
2025-07-28 21:21:10 +0000

    update

--- internal/wasm/dom/media_devices.go
+++ internal/wasm/dom/media_devices.go
@@ -6,6 +6,16 @@ type (
 	}
 )
 
+func (m *MediaDevices) GetUserMedia(constraints map[string]any) (*MediaStream, error) {
+	v, err := m.Call("getUserMedia", constraints).Await()
+	if err != nil {
+		return nil, err
+	}
+	return &MediaStream{
+		DOM: v,
+	}, nil
+}
+
 func GetMediaDevices() *MediaDevices {
 	return GetNavigator().MediaDevices()
 }

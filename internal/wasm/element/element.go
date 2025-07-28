319ee5ce84801ae8dfc57a6d1c0b2f32471513e7
Tarcisio Oliveira <t@rcis.io>
2025-07-28 23:10:22 +0000

    update

--- internal/wasm/element/element.go
+++ internal/wasm/element/element.go
@@ -9,3 +9,17 @@ type (
 		dom *dom.DOM
 	}
 )
+
+func (e *Element) SetId(id string) {
+	e.dom.Set("id", id)
+}
+
+func (e *Element) SetClassName(className string) {
+	e.dom.Set("className", className)
+}
+
+func Create(tagName string) *Element {
+	return &Element{
+		dom: dom.GetDocument().CreateElement(tagName),
+	}
+}

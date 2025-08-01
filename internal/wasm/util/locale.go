756b1d1d14157768b2d2b926390111e946b32820
Tarcisio Oliveira <t@rcis.io>
2025-08-01 02:45:49 +0000

    update

--- internal/wasm/util/locale.go
+++ internal/wasm/util/locale.go
@@ -1,10 +1,14 @@
 package util
 
 type (
-	i18n struct {
+	locale struct {
 		App            string `json:"app"`
 		AppDescription string `json:"appDescription"`
 		AppDevelopedBy string `json:"appDevelopedBy"`
 		AppVersion     string `json:"appVersion"`
 	}
+)
+
+var (
+	currentLocale locale
 )

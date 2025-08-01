c01f16fdad6d2eb1e8cd9376a4c6daf305c655a2
Tarcisio Oliveira <t@rcis.io>
2025-08-01 02:20:45 +0000

    update

--- internal/wasm/util/i18n.go
+++ internal/wasm/util/i18n.go
@@ -1,1 +1,10 @@
 package util
+
+type (
+	i18n struct {
+		App            string `json:"app"`
+		AppDescription string `json:"appDescription"`
+		AppDevelopedBy string `json:"appDevelopedBy"`
+		AppVersion     string `json:"appVersion"`
+	}
+)

042399fd9e29cb3ce9dfc8591b8e695dd6b9b8a4
Tarcisio Oliveira <t@rcis.io>
2025-08-01 06:21:38 +0000

    update

--- internal/wasm/page/home.go
+++ internal/wasm/page/home.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"dummy_ai/internal/wasm/dom"
+	"dummy_ai/internal/wasm/util"
 )
 
 func main() {
 	h2 := dom.GetDocument().CreateElement("h2")
-	h2.Set("innerText", "/home.wasm")
+	h2.Set("innerText", util.App())
 
 	body := dom.GetDocument().Get("body")
 	body.Call("appendChild", h2)

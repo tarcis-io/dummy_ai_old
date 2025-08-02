fbec8bd47384fc7bfbc91d9bc6837bc7e7bc769e
Tarcisio Oliveira <t@rcis.io>
2025-08-02 03:30:12 +0000

    update

--- internal/wasm/dom/document.go
+++ internal/wasm/dom/document.go
@@ -1,15 +1,18 @@
 package dom
 
 type (
+	// Document represents the JavaScript Document object.
 	Document struct {
 		*DOM
 	}
 )
 
+// CreateElement creates a new HTML element with the specified tag name and returns it as a new *DOM object.
 func (d *Document) CreateElement(tagName string) *DOM {
 	return d.Call("createElement", tagName)
 }
 
+// GetDocument returns the web page's current Document object.
 func GetDocument() *Document {
 	return GetWindow().Document()
 }
--- internal/wasm/dom/local_storage.go
+++ internal/wasm/dom/local_storage.go
@@ -1,15 +1,18 @@
 package dom
 
 type (
+	// LocalStorage represents the JavaScript LocalStorage object.
 	LocalStorage struct {
 		*DOM
 	}
 )
 
+// SetItem sets the value of the specified key in the LocalStorage object.
 func (l *LocalStorage) SetItem(key, value string) {
 	l.Call("setItem", key, value)
 }
 
+// GetItem retrieves the value of the specified key from the LocalStorage object.
 func (l *LocalStorage) GetItem(key string) (string, bool) {
 	v := l.Call("getItem", key)
 	if v.Truthy() {
@@ -18,6 +21,7 @@ func (l *LocalStorage) GetItem(key string) (string, bool) {
 	return "", false
 }
 
+// GetLocalStorage returns the web page's current LocalStorage object.
 func GetLocalStorage() *LocalStorage {
 	return GetWindow().LocalStorage()
 }

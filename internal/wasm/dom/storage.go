cf11cfa70d9aaf153708c9e9dd299fe390026bd6
Tarcisio Oliveira <t@rcis.io>
2025-08-03 06:32:33 +0000

    update

--- internal/wasm/dom/storage.go
+++ internal/wasm/dom/storage.go
@@ -1,15 +1,18 @@
 package dom
 
 type (
+	// Storage represents the JavaScript Storage object.
 	Storage struct {
 		*DOM
 	}
 )
 
+// SetItem sets the value for the given key in the Storage object.
 func (s *Storage) SetItem(key, value string) {
 	s.Call("setItem", key, value)
 }
 
+// GetItem returns the value for the given key in the Storage object.
 func (s *Storage) GetItem(key string) (string, bool) {
 	v := s.Call("getItem", key)
 	if v.Truthy() {
@@ -18,10 +21,12 @@ func (s *Storage) GetItem(key string) (string, bool) {
 	return "", false
 }
 
+// GetLocalStorage returns the current local Storage object.
 func GetLocalStorage() *Storage {
 	return GetWindow().LocalStorage()
 }
 
+// GetSessionStorage returns the current session Storage object.
 func GetSessionStorage() *Storage {
 	return GetWindow().SessionStorage()
 }
--- internal/wasm/dom/window.go
+++ internal/wasm/dom/window.go
@@ -1,35 +1,41 @@
 package dom
 
 type (
+	// Window represents the JavaScript Window object.
 	Window struct {
 		*DOM
 	}
 )
 
+// Document returns the Window's Document object.
 func (w *Window) Document() *Document {
 	return &Document{
 		DOM: w.Get("document"),
 	}
 }
 
+// Navigator returns the Window's Navigator object.
 func (w *Window) Navigator() *Navigator {
 	return &Navigator{
 		DOM: w.Get("navigator"),
 	}
 }
 
+// LocalStorage returns the Window's local Storage object.
 func (w *Window) LocalStorage() *Storage {
 	return &Storage{
 		DOM: w.Get("localStorage"),
 	}
 }
 
+// SessionStorage returns the Window's session Storage object.
 func (w *Window) SessionStorage() *Storage {
 	return &Storage{
 		DOM: w.Get("sessionStorage"),
 	}
 }
 
+// GetWindow returns the current Window object.
 func GetWindow() *Window {
 	return &Window{
 		DOM: GetGlobal(),

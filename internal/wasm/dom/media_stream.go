d3fa8e1c31674bfa6df16edccf5ed4c0756e53f1
Tarcisio Oliveira <t@rcis.io>
2025-08-03 06:05:52 +0000

    update

--- internal/wasm/dom/media_stream.go
+++ internal/wasm/dom/media_stream.go
@@ -1,11 +1,13 @@
 package dom
 
 type (
+	// MediaStream represents the JavaScript MediaStream object.
 	MediaStream struct {
 		*DOM
 	}
 )
 
+// GetUserMedia requests access to the user's media devices.
 func GetUserMedia(constraints map[string]any) (*MediaStream, error) {
 	return GetMediaDevices().GetUserMedia(constraints)
 }

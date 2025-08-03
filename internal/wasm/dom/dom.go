1f438025e47f34e9d0a1e8862add1a3487c2ed78
Tarcisio Oliveira <t@rcis.io>
2025-08-03 03:41:38 +0000

    update

--- internal/wasm/dom/dom.go
+++ internal/wasm/dom/dom.go
@@ -1,3 +1,5 @@
+// Package dom provides a Go-friendly wrapper
+// for interacting with the JavaScript DOM.
 package dom
 
 import (
@@ -7,47 +9,58 @@ import (
 )
 
 type (
+	// DOM represents a JavaScript DOM object.
 	DOM struct {
 		jsValue js.Value
 	}
 )
 
+// Bool returns the DOM object as a boolean.
 func (d *DOM) Bool() bool {
 	return d.jsValue.Bool()
 }
 
+// String returns the DOM object as a string.
 func (d *DOM) String() string {
 	return d.jsValue.String()
 }
 
+// Int returns the DOM object as an integer.
 func (d *DOM) Int() int {
 	return d.jsValue.Int()
 }
 
+// Float returns the DOM object as a float.
 func (d *DOM) Float() float64 {
 	return d.jsValue.Float()
 }
 
+// Truthy returns the "truthiness" of the DOM object.
 func (d *DOM) Truthy() bool {
 	return d.jsValue.Truthy()
 }
 
+// Get retrieves a property from the DOM object.
 func (d *DOM) Get(property string) *DOM {
 	return &DOM{
 		jsValue: d.jsValue.Get(property),
 	}
 }
 
+// Set sets a property on the DOM object.
 func (d *DOM) Set(property string, value any) {
 	d.jsValue.Set(property, unwrapValue(value))
 }
 
+// Call invokes a method on the DOM object.
 func (d *DOM) Call(method string, args ...any) *DOM {
 	return &DOM{
 		jsValue: d.jsValue.Call(method, unwrapValues(args)...),
 	}
 }
 
+// Await awaits the resolution of the DOM object.
+// It must be called on a promise-returning method.
 func (d *DOM) Await() (*DOM, error) {
 	valueChan := make(chan *DOM)
 	errorChan := make(chan error)
@@ -64,12 +77,14 @@ func (d *DOM) Await() (*DOM, error) {
 	}
 }
 
+// GetGlobal returns the global object.
 func GetGlobal() *DOM {
 	return &DOM{
 		jsValue: js.Global(),
 	}
 }
 
+// Fetch fetches a URL and unmarshals the response into a struct.
 func Fetch[T any](url string) (*T, error) {
 	v, err := GetGlobal().Call("fetch", url).Await()
 	if err != nil {
@@ -87,6 +102,7 @@ func Fetch[T any](url string) (*T, error) {
 	return &t, nil
 }
 
+// unwrapValue unwraps a value into a JavaScript value.
 func unwrapValue(value any) any {
 	v, ok := value.(*DOM)
 	if ok {
@@ -95,6 +111,7 @@ func unwrapValue(value any) any {
 	return value
 }
 
+// unwrapValues unwraps a slice values into a slice of JavaScript values.
 func unwrapValues(values []any) []any {
 	unwrappedValues := make([]any, len(values))
 	for i, v := range values {
@@ -103,6 +120,7 @@ func unwrapValues(values []any) []any {
 	return unwrappedValues
 }
 
+// onFulfilledCallback returns a JavaScript function that is called when a promise is fulfilled.
 func onFulfilledCallback(valueChan chan<- *DOM) js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) any {
 		valueChan <- &DOM{
@@ -112,6 +130,7 @@ func onFulfilledCallback(valueChan chan<- *DOM) js.Func {
 	})
 }
 
+// onRejectedCallback returns a JavaScript function that is called when a promise is rejected.
 func onRejectedCallback(errorChan chan<- error) js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) any {
 		errorChan <- errors.New(args[0].String())

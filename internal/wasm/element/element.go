b571b83ba84efc453889eeb1716323e68ed37034
Tarcisio Oliveira <t@rcis.io>
2025-08-03 06:57:51 +0000

    update

--- internal/wasm/element/element.go
+++ internal/wasm/element/element.go
@@ -1,3 +1,5 @@
+// Package element provides a Go-friendly wrapper
+// for interacting with HTML elements.
 package element
 
 import (
@@ -5,19 +7,23 @@ import (
 )
 
 type (
+	// Element represents an HTML element.
 	Element struct {
 		dom *dom.DOM
 	}
 )
 
+// SetId sets the id attribute of the element.
 func (e *Element) SetId(id string) {
 	e.dom.Set("id", id)
 }
 
+// SetClassName sets the className attribute of the element.
 func (e *Element) SetClassName(className string) {
 	e.dom.Set("className", className)
 }
 
+// Create creates a new HTML element with the specified tag name.
 func Create(tagName string) *Element {
 	return &Element{
 		dom: dom.GetDocument().CreateElement(tagName),

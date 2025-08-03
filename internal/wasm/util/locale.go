916cfb864b1ba3cd0eb7dd4f3284c71967707d69
Tarcisio Oliveira <t@rcis.io>
2025-08-03 22:38:14 +0000

    update

--- internal/wasm/util/locale.go
+++ internal/wasm/util/locale.go
@@ -7,6 +7,7 @@ import (
 )
 
 type (
+	// locale holds the localized strings for the application.
 	locale struct {
 		App            string `json:"app"`
 		AppDescription string `json:"appDescription"`
@@ -16,25 +17,31 @@ type (
 )
 
 var (
+	// currentLocale holds the currently loaded locale.
 	currentLocale = fetchCurrentLocale()
 )
 
+// App returns the localized name of the application.
 func App() string {
 	return currentLocale.App
 }
 
+// AppDescription returns the localized description of the application.
 func AppDescription() string {
 	return currentLocale.AppDescription
 }
 
+// AppDevelopedBy returns the localized "developed by" credit.
 func AppDevelopedBy() string {
 	return currentLocale.AppDevelopedBy
 }
 
+// AppVersion returns the localized version of the application.
 func AppVersion() string {
 	return currentLocale.AppVersion
 }
 
+// fetchCurrentLocale fetches the current locale for the currently selected language.
 func fetchCurrentLocale() *locale {
 	v, _ := dom.Fetch[locale](fmt.Sprintf("/locale/%s.json", currentLanguage.code))
 	return v

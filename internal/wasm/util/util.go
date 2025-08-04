0ba203fda709bcb3976578dbd1e885a5f5bd5357
Tarcisio Oliveira <t@rcis.io>
2025-08-04 00:23:58 +0000

    update

--- internal/wasm/util/util.go
+++ internal/wasm/util/util.go
@@ -1,5 +1,8 @@
+// Package util provides shared utilities for the application.
 package util
 
+// init runs on startup.
+// It configures the package's initial state.
 func init() {
 	currentLanguage = LookupLanguage()
 	currentLocale = FetchCurrentLocale()

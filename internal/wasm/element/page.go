b79fac2be5d33037aeca82d3e08bdc97c5f27cc9
Tarcisio Oliveira <t@rcis.io>
2025-08-05 03:11:47 +0000

    update

--- internal/wasm/element/page.go
+++ internal/wasm/element/page.go
@@ -1,7 +1,20 @@
 package element
 
+import (
+	"dummy_ai/internal/wasm/util"
+)
+
 func CreatePage(content *Element) *Element {
 	page := Create("div")
+	page.AppendChild(CreatePageHeader())
 	page.AppendChild(content)
 	return page
+}
+
+func CreatePageHeader() *Element {
+	headerName := Create("cds-header-name")
+	headerName.SetTextContent(util.App())
+	header := Create("cds-header")
+	header.AppendChild(headerName)
+	return header
 }

2f72c267aee6ec4b61cbd7c02efe5093349cf322
Tarcisio Oliveira <t@rcis.io>
2025-08-05 05:06:10 +0000

    update

--- internal/wasm/element/page.go
+++ internal/wasm/element/page.go
@@ -6,6 +6,15 @@ import (
 
 func CreatePage(content *Element) *Element {
 	div := Create("div")
+	div.Set("className", "cds-theme-zone-g100")
+	div.SetStyle("fontFamily", "'IBM Plex Sans', 'Helvetica Neue', Arial, sans-serif")
+	div.SetStyle("position", "fixed")
+	div.SetStyle("top", 0)
+	div.SetStyle("right", 0)
+	div.SetStyle("bottom", 0)
+	div.SetStyle("left", 0)
+	div.SetStyle("overflowY", "auto")
+	div.SetStyleProperty("--cds-background", "black")
 	div.AppendChild(createPageHeader())
 	div.AppendChild(createPageContentContainer(content))
 	return div
@@ -25,6 +34,8 @@ func createPageHeaderName() *Element {
 
 func createPageContentContainer(content *Element) *Element {
 	div := Create("div")
+	div.SetStyle("margin", "var(--cds-spacing-03)")
+	div.SetStyle("paddingTop", "var(--cds-spacing-09)")
 	div.AppendChild(content)
 	return div
 }

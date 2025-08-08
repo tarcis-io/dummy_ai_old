d677f7a6f2d4f142ee6566e48d1355cd43dc5c5c
Tarcisio Oliveira <t@rcis.io>
2025-08-08 03:37:35 +0000

    update

--- internal/wasm/util/util.go
+++ internal/wasm/util/util.go
@@ -3,6 +3,9 @@ package util
 
 // init runs on startup.
 // It configures the package's initial state.
+// 1. The current language.
+// 2. The current locale.
+// 3. The current theme.
 func init() {
 	currentLanguage = LookupLanguage()
 	currentLocale = FetchLocale(currentLanguage)
